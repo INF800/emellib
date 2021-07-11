ea84bcf2bdc5f09a6725b82a34574031aef925c0
Asapanna Rakesh <rakeshark22@gmail.com>
2021-07-11 13:36:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+export const testMe = () => { console.log('i am being tested') }
+testMe() // run `node index.js` to see `i am being tested` as output
+
+// make sure it works when you use it using require or import statement.
+// So that you can use your created npm package inside any project. 
+// Run:
+// $ npm link
+
